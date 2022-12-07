4b233f1a863e09fc5f207b03d1760f1bc490db24
Vincent1230 <516879257@qq.com>
2022-12-07 22:03:37 +0800

    一二

--- 期中报告_old/generation.py
+++ 期中报告_old/generation.py
@@ -6,6 +6,7 @@ generation 数据生成部分
 
 import numpy as np
 import pandas as pd
+from pathlib import Path
 
 NAMES = ['张三', '李四', '王五']
 COURSES = ['语文', '数学', '英语', '物理', '化学', '生物', '政治', '历史', '地理']
@@ -14,6 +15,12 @@ LIMIT = [60, 100]
 
 def generate():
     """生成数据并存入 Excel 文件"""
+    dir = Path(__file__).parent
     data = np.random.randint(LIMIT[0], LIMIT[1], (len(NAMES), len(COURSES)))
     df = pd.DataFrame(data, index=NAMES, columns=COURSES)
-    df.to_excel('data.xlsx')
+    df.to_excel(dir / 'data.xlsx')
+    print('数据生成成功!')
+
+
+if __name__ == '__main__':
+    generate()
